6e311d011254e3bba10def8eab2e7fe77d52bb32
Nikhilcharan-dev <chintufru143@gmail.com>
2024-12-20 03:22:38 +0530

    initial commits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
 
-// import NavBar from './ui/navbar/NavBar';
-// import Quote from './ui/quote/Quote'
-import LoginPage from './ui/loginpage/LoginPage';
+import LoginPage from "./ui/loginpage/LoginPage";
 
 function App() {
+
   return (
-    <div className="App">
+    <>
       <LoginPage/>
-      {/* <NavBar/> */}
-    </div>
+    </>
   );
-}
+};
 
 export default App;
