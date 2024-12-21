3b620e77df309bc0e3313ac7290d182147650f5d
Nikhilcharan-dev <chintufru143@gmail.com>
2024-12-21 13:17:13 +0530

    middle ware enabled

--- server/server.js
+++ server/server.js
@@ -3,36 +3,34 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/user.js';
-import quoteRoutes from './routes/quotes.js'
+import quoteRoutes from './routes/quotes.js';
 import infoRoutes from './routes/credentials.js';
 import connectDB from './config/db.js';
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true })); 
 
 connectDB();
 
-app
-    .route('/')
-    .get((req, res) => {
-        const welcome = `
-            <h1> Welcome to Backend Server </h1>
-            <p> Nikhil Charan -- </p>
-        `;
-        return res.send(welcome);
-    });
+app.route('/').get((req, res) => {
+    const welcome = `
+        <h1> Welcome to Backend Server </h1>
+        <p> Nikhil Charan -- </p>
+    `;
+    return res.send(welcome);
+});
 
 app.use('/api/oauth', authRoutes);
 app.use('/api/random', quoteRoutes);
 app.use('/api/users', infoRoutes);
 app.use('/api/users', userRoutes);
 
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running at Port: http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running at Port: http://localhost:${PORT}`);
 });
