ece91de4ffe6bc5a0a1ad9585165061e75303409
Nikhilcharan-dev <chintufru143@gmail.com>
2024-12-22 03:30:29 +0530

    hmmm

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
 router.post("/filter", async (req, res) => {
     try {
 
-        const { difficulty, topics, platform } = req.body.topics;
+        const { difficulty, topics, platform } = req.body.filters;
 
         if (!filters) {
             return res.status(400).json({ message: "No filters provided" });
