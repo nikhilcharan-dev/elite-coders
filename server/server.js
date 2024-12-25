cb5b7a63b858897c75f49dfba69092eec3a76c3b
Nikhilcharan-dev <chintufru143@gmail.com>
2024-12-25 20:12:34 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // origin whitelist
-const allowedOrigins = ['http://localhost:5173', 'https://elite-coders-nikirus-projects.vercel.app/'];
+const allowedOrigins = ['http://localhost:5173', 'https://elite-coders-nikirus-projects.vercel.app', 'https://elite-coders-xii.vercel.app'];
 
 const corsOptions = {
     origin: (origin, callback) => {
