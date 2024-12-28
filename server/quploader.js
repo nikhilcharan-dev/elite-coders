d5389e5528c24c599a9b1953cb8671615fdf4dcb
Nikhilcharan-dev <chintufru143@gmail.com>
2024-12-29 00:32:49 +0530

    added contributor

--- server/quploader.js
+++ server/quploader.js
@@ -14,24 +14,25 @@ const Question = mongoose.model('QuestionBank', questionSchema);
 
 async function collectQuestionData() {
     const name = readlineSync.question('Enter the question name: ');
-    let difficulty = readlineSync.question('Enter the difficulty (Easy, Medium, Hard, S[SuperHard]): ');
-    const platformName = readlineSync.question('Enter the Paltform (LeetCode, GFG, CodeForces, CodeChef): ')
+    let difficulty = readlineSync.question('Enter the difficulty (Easy(e), Medium(m), Hard(h), S[SuperHard]): ');
+    const platform = readlineSync.question('Enter the Platform (LeetCode(l), GFG(g), CodeForces(cf), CodeChef(c)): ')
     const link = readlineSync.question('Enter the Link: ');
 
     // Standardize difficulty input
     difficulty = difficulty.toLowerCase() === 'e' ? 'Easy' : 
                 difficulty.toLowerCase() === 'm' ? 'Medium' : 
-                difficulty.toLowerCase() === 'h' ? 'Hard' : difficulty;
+                difficulty.toLowerCase() === 'h' ? 'Hard' : 
+                difficulty.toLowerCase() === 's' ? 'S' : difficulty;
 
     // Standardize platform input
-    // let platformName;
-    // switch(platform.toLowerCase()) {
-    //     case 'l': platformName = 'LeetCode'; break;
-    //     case 'g': platformName = 'GFG'; break;
-    //     case 'c': platformName = 'CodeChef'; break;
-    //     case 'h': platformName = 'HackerRank'; break;
-    //     default: platformName = platform; break;
-    // }
+    let platformName;
+    switch(platform.toLowerCase()) {
+        case 'l': platformName = 'LeetCode'; break;
+        case 'g': platformName = 'GFG'; break;
+        case 'c': platformName = 'CodeChef'; break;
+        case 'cf': platformName = 'CodeForces'; break;
+        default: platformName = platform; break;
+    }
 
     // Multiple topics input (comma separated)
     const topicsInput = readlineSync.question('Enter the topics (comma separated): ');
@@ -58,10 +59,10 @@ async function collectQuestionData() {
 async function collectQuestions() {
     try {
         await connectDB();  // Connect to the database once
-        const numQuestions = 3333;
+        const numQuestions = readlineSync.questionInt('Enter the number of questions you want to collect: ');
         
         // Loop to collect and save questions
-        for (let i = 72; i < numQuestions; i++) { // 100 questions
+        for (let i = 0; i < numQuestions; i++) { // 100 questions
             console.log(`Collecting question ${i + 1}/${numQuestions}`);
             await collectQuestionData();  // Wait for each question to be saved before continuing
         }
